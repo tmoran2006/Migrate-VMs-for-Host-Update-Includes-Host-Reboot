4330b9dc73797ed35e1318b25ea898271a77cba6
tmoran2006 <125475885+tmoran2006@users.noreply.github.com>
2024-03-12 12:39:38 -0500

    Update Migrate VMs for Host Update.ps1
    Edited the original commit to be more generalized.

--- Migrate VMs for Host Update.ps1
+++ Migrate VMs for Host Update.ps1
@@ -1,8 +1,10 @@
+#IP Addresses of vCenter 
 $prodVCSA = <production VCSA>
 $drVCSA = <dr VCSA>
 
-$prodHosts = <Enter hosts using comma separated values. Include quotation marks around each entry>
-$drHosts = <Enter hosts using comma separated values. Include quotation marks around each entry>
+#Initializes Hashtable for Hosts and VMs
+$HostIDTable = @{}
+$VMIDTable = @{}
 
 $readyState = "Connected"
 $verifyCount = 0
@@ -18,8 +20,8 @@ Write-Host "################################`n" -ForegroundColor Cyan
 $num = read-host "Enter Selection"
 
 switch($num){
-    1 {cls;$vcsa = $prodVCSA;$hosts = $prodHosts;"`n`nStarting at PROD`n"}
-    2 {cls;$vcsa = $drVCSA;$hosts = $drHosts;"`n`nStarting DR`n"}
+    1 {cls;$vcsa = $prodVCSA;"`n`nStarting at PROD`n"}
+    2 {cls;$vcsa = $drVCSA;"`n`nStarting DR`n"}
     Default{
         cls
         Write-Host "`n`n`n################################`n" -ForegroundColor Yellow
@@ -31,16 +33,25 @@ switch($num){
     }
 }
 
-#Establishes a connection with the vcenter server
+#Connects to vCenter Server from your selection
 Connect-VIServer -Server $vcsa
 
-foreach($h in $hosts){
+#Queries vCenter for each host and the ID assigned to it and inserts that information into the $HostIDTable hashtable
+Get-VMHost | Select-Object -Property Name,ID | ForEach-Object {$HostIDTable[$_.Id] = $_.name}
+
+#Queries vCenter server for All VMs and what host they are on and inserts that information into the $VMIDTable hashtable
+Get-VM | Select-Object -Property Name,VMHostId | ForEach-Object {$VMIDTable[$_.Name] = $_.VMHostId}
+
+
+foreach($h in $HostIDTable.Values){
        
     $j = 0
-    $destHosts = $hosts | where {$_ -ne $h}
+    $destHosts = $HostIDTable.Values | where {$_ -ne $h}
     
+    #Gets the currently hosted VM's of the current host
     $vms = get-vmhost -Name $h | Get-VM
     
+    #Moves VMs to other hosts besides the current one in round robin order
     foreach($vm in $vms){
         $vmName = $vm.name
         
@@ -51,19 +62,19 @@ foreach($h in $hosts){
 
         Write-Host "Moving $vmName to $tgtHost" -ForegroundColor Yellow
        
-        move-vm -VM $vmName -Destination $destHosts[$j] -VMotionPriority High | Out-Null
+        move-vm -VM $vmName -Destination $tgtHost -VMotionPriority High | Out-Null
         
-        $j = $j + 1
+        $j++
     }
 
     
     #Puts VMHost into Maintenance Mode
-    Write-Host "Putting $h into maintenance mode...`n" -ForegroundColor Cyan
-    Get-VMHost -Name $h | set-vmhost -State Maintenance |out-null
+    Write-Host "`nPutting $h into maintenance mode...`n" -ForegroundColor Cyan
+    Get-VMHost -Name $h | set-vmhost -State Maintenance | Out-Null
 
     #Starts SSH service
     Write-Host "Starting SSH service on $h...`n" -ForegroundColor Magenta
-    Get-VMHost -name $h | Get-VMHostService | where {$_.key -eq 'TSM-SSH'} | Start-VMHostService -Confirm:$false | out-null
+    Get-VMHost -name $h | Get-VMHostService | where {$_.key -eq 'TSM-SSH'} | Start-VMHostService -Confirm:$false | Out-Null
 
     #Prompt for user to press enter once ESXi Host update is complete
     Read-Host -Prompt "Press enter to reboot VMHost"
@@ -71,14 +82,14 @@ foreach($h in $hosts){
     #Restarts VMHost after updates have been applied
     Restart-VMHost -VMHost $h -Confirm:$false
 
-    #Begins sleep timer and tests connection to host to confirm host is back online
+    #Begins sleep timer and tests connection to host to confirm host is back online before continuing
     
     Start-Sleep -Seconds 80
        
     do{
         $conStatus = Test-Connection -ComputerName $h -Quiet -Count 1
         Start-sleep -Seconds 5
-        Write-host $conStatus
+        Write-host "Host has not reconnected yet..." -foregroundcolor yellow
     }while($conStatus -eq $false)
     
     start-sleep -Seconds 70
@@ -100,14 +111,12 @@ foreach($h in $hosts){
 
     #Moves all VMs back to original host
     foreach($vm in $vms){
-        $vmName = $vm.name
-        write-host "Moving $vmName back to $h `n" -ForegroundColor Green
-        Move-VM -VM $vmName -Destination $h -VMotionPriority High |Out-Null
+        write-host "Moving "$vm.Name" back to $h `n" -ForegroundColor Green
+        Move-VM -VM $vm.Name -Destination $h -VMotionPriority High |Out-Null
     }
 
     #Verify All VM's are back home
     if($verifyCount -eq ($vms.count - 1)){
-        
         $verifyCount = 0
     }else{
         $verifyCount++
@@ -115,17 +124,23 @@ foreach($h in $hosts){
 }
 
 #verifying vm's are all on the correct host before ending
-foreach($h in $hosts){
-    Write-Host "Starting on $h" -ForegroundColor Green
-
-    $vms = import-csv -Path 
-    
-    foreach($vm in $vms){
-        $vmName = $vm.name
+foreach($vm in $VMIDTable.Keys){
         
-        Write-Host "Moving $vmName to $h" -ForegroundColor Yellow
+     Write-Host "Moving $vm to"$HostIDTable[$VMIDTable["$vm"]] -ForegroundColor Yellow
        
-        move-vm -VM $vmName -Destination $h -VMotionPriority High -RunAsync |Out-Null
-        
-    }
+     move-vm -VM $vm -Destination $HostIDTable[$VMIDTable["$vm"]] -VMotionPriority High -RunAsync |Out-Null
 }
+
+
+#stops both SSH and BASH
+Get-VMHost | Foreach {
+    Write-Host "Stopping SSH service on $_"
+	Stop-VMHostService -HostService ($_ | Get-VMHostService | Where {$_.Key -eq "TSM-SSH"}) -Confirm:$false |Out-Null
+    Write-Host "Stopping bash service on $_"
+    Stop-VMHostService -HostService ($_ | Get-VMHostService | Where {$_.Key -eq "TSM"}) -Confirm:$false | Out-Null
+}
+
+$VMIDTable = $null
+$HostIDTable = $null
+
+Disconnect-VIServer -Confirm:$false
